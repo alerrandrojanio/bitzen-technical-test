7f547f32373a1d6e02536300ae9839f121762aa3
Alerrandro Janio <alerrandro.janio07@gmail.com>
2025-04-04 10:44:22 -0300

    refactor: remoção de settings não utilizada

--- src/MeetingRooms.Infrastructure/Configurations/AuthSettings.cs
+++ src/MeetingRooms.Infrastructure/Configurations/AuthSettings.cs
@@ -7,6 +7,4 @@ public class AuthSettings
     public string Issuer { get; set; } = string.Empty;
 
     public string Audience { get; set; } = string.Empty;
-
-    public string PublicRoutes { get; set; } = string.Empty;
 }
